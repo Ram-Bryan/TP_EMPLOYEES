d6bc418d402f40a577ccd4e522b33da36b506b7b
Bryan <ramboliarisoabryan1@gmail.com>
2026-07-08 08:39:34 +0300

    connection changed + comprehension

--- employees.sql
+++ employees.sql
@@ -125,3 +125,25 @@ source load_salaries2.dump ;
 source load_salaries3.dump ;
 
 source show_elapsed.sql ;
+
+-- SELECT 'LOADING departments' as 'INFO';
+-- source /opt/lampp/htdocs/S4/SI/src-tp-real-bdd/test_db-1.0.7/test_db/load_departments.dump ;
+
+-- SELECT 'LOADING employees' as 'INFO';
+-- source /opt/lampp/htdocs/S4/SI/src-tp-real-bdd/test_db-1.0.7/test_db/load_employees.dump ;
+
+-- SELECT 'LOADING dept_emp' as 'INFO';
+-- source /opt/lampp/htdocs/S4/SI/src-tp-real-bdd/test_db-1.0.7/test_db/load_dept_emp.dump ;
+
+-- SELECT 'LOADING dept_manager' as 'INFO';
+-- source /opt/lampp/htdocs/S4/SI/src-tp-real-bdd/test_db-1.0.7/test_db/load_dept_manager.dump ;
+
+-- SELECT 'LOADING titles' as 'INFO';
+-- source /opt/lampp/htdocs/S4/SI/src-tp-real-bdd/test_db-1.0.7/test_db/load_titles.dump ;
+
+-- SELECT 'LOADING salaries' as 'INFO';
+-- source /opt/lampp/htdocs/S4/SI/src-tp-real-bdd/test_db-1.0.7/test_db/load_salaries1.dump ;
+-- source /opt/lampp/htdocs/S4/SI/src-tp-real-bdd/test_db-1.0.7/test_db/load_salaries2.dump ;
+-- source /opt/lampp/htdocs/S4/SI/src-tp-real-bdd/test_db-1.0.7/test_db/load_salaries3.dump ;
+
+-- source /opt/lampp/htdocs/S4/SI/src-tp-real-bdd/test_db-1.0.7/test_db/show_elapsed.sql ;
